c57b38f9b5b9cc32aab5d70e0bbe07ad322a5b71
suraj chauhan <surajchauhan@surajs-MacBook-Pro.local>
2025-08-23 04:22:01 +0530

    renamed app name

--- ios/TodoApp/AppDelegate.swift
+++ ios/TodoApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "surajDemo",
+      withModuleName: "Todo App",
       in: window,
       launchOptions: launchOptions
     )
